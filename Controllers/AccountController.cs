8b74cec3c1a667df49df74a9a97da57f2857a33c
Ines CHAOUCH <chaouchines00@gmail.com>
2025-02-08 19:12:38 +0100

    sections 1-7

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class AccountController(DataContext context, ITokenService tokenService) : BaseApiController
+    {
+    [HttpPost("register")]
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerdto)
+    { 
+        if (await UserExists(registerdto.Username)) return BadRequest("Username is taken");
+
+        using var hmac = new HMACSHA512(); // put using otherwise garbage collector will delete this
+
+        var user = new AppUser
+        {
+            UserName = registerdto.Username.ToLower(),
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerdto.Password)),
+            PasswordSalt = hmac.Key
+        };
+
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        return new UserDto
+       {
+        Username = user.UserName,
+        Token = tokenService.CreateToken(user),
+       };
+    }
+    public async Task<bool> UserExists(string username){
+        return await context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
+    }
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDto>> Login(LoginDto logindto)
+    {
+       var user = await context.Users.FirstOrDefaultAsync(x => x.UserName == logindto.UserName.ToLower());
+       
+       if (user == null) return Unauthorized("invalid username or password");
+
+       using var hmac = new HMACSHA512(user.PasswordSalt);
+
+       var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(logindto.Password));
+
+       for (int i = 0; i<computedHash.Length; i++)
+       {
+        if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("invalid Password");
+       }
+
+       return new UserDto
+       {
+        Username = user.UserName,
+        Token = tokenService.CreateToken(user),
+       };
+    }
+
+
+    }
+}
---
+++ Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)//why do we use this in here ?
+        {  
+
+            services.AddControllers();
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(config.GetConnectionString("DefaultConection"));
+            });
+            services.AddCors();
+            services.AddScoped<ITokenService, TokenService>(); //builder.Services.AddScoped<TokenService>(); this would work just fine but the other one is a common way 
+            
+            return services;
+        }
+    }
+}
