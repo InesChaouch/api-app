4ff272119d2cbb889e52d32442d664784796fe29
Ines CHAOUCH <chaouchines00@gmail.com>
2025-02-23 19:20:44 +0100

    Adding the Messaging feature

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -12,26 +12,40 @@ namespace API.Data
     {
         public DbSet<AppUser> Users { get; set; }
         public DbSet<UserLike> Likes { get; set; }
+        public DbSet<Message> Messages { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
 
             builder.Entity<UserLike>()
-            .HasKey(k => new {k.SourceUserId, k.TargetUserId});
+            .HasKey(k => new {k.SourceUserId, k.TargetUserId}); //we didn't add the same thing for msg cause because each message requires a unique Id to allow multiple messages between the same users.
 
             builder.Entity<UserLike>()
-            .HasOne(s => s.SourceUser)
-            .WithMany(l => l.LikedUsers)
-            .HasForeignKey(l => l.SourceUserId)
-            .OnDelete(DeleteBehavior.Cascade);
+                .HasOne(s => s.SourceUser)
+                .WithMany(l => l.LikedUsers)
+                .HasForeignKey(l => l.SourceUserId)
+                .OnDelete(DeleteBehavior.Cascade);
 
              builder.Entity<UserLike>()
-            .HasOne(s => s.TargetUser)
-            .WithMany(l => l.LikedByUsers)
-            .HasForeignKey(l => l.TargetUserId)
-            .OnDelete(DeleteBehavior.Cascade);
+                .HasOne(s => s.TargetUser)
+                .WithMany(l => l.LikedByUsers)
+                .HasForeignKey(l => l.TargetUserId)
+                .OnDelete(DeleteBehavior.Cascade);
 
+            builder.Entity<Message>()
+                .HasOne(s => s.Recipient)
+                .WithMany(l => l.MessagesReceived)
+                .HasForeignKey(l => l.RecipientId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Message>()
+                .HasOne(s => s.Sender)
+                .WithMany(l => l.MessagesSent)
+                .HasForeignKey(l => l.SenderId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            
 
 
         }
